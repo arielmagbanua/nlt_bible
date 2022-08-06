d63789a321f75a1ec940416d4e2fe25a2fca4a99
unknown <magbanua.c.ariel@gmail.com>
2022-08-06 13:01:23 +0800

    Added example test data.

--- test/nlt_bible_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:nlt_bible/nlt_bible.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
---
+++ test/test_data.dart
@@ -0,0 +1,25 @@
+const samplePassagesResult = r'''
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+	<title>NLT API</title>
+	<link rel="stylesheet" href="https://api.nlt.to/content/nlt-api-css?vers=1.04" />
+</head>
+
+<body>
+	<div id="bibletext" class=" NLT NLT BibleText section">
+		<section>
+			<h2 class="bk_ch_vs_header">John 3:16, NLT</h2>
+			<verse_export orig="john_3_16" bk="john" ch="3" vn="16">
+				<p class="body">
+					<span class="vn">16</span><span class="red">“For this is how God loved the world: He gave<a class="a-tn">*</a><span class="tn"><span class="tn-ref">3:16</span>
+					Or <em>For God loved the world so much that he gave.</em></span> his one and only Son, so that
+					everyone who believes in him will not perish but have eternal life.</span>
+			</verse_export>
+		</section>
+	</div>
+</body>
+
+</html>
+''';
