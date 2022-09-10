44b8e1cebe3ca5f562e01749ba2fa774997ccee1
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2022-09-10 20:53:33 +0800

    Fixed Missing API Key and Source Code Formatting Improvements. (#10)
    * Fixed the missing API key.
    
    * Improved the formats.
    
    * Improved the code formatting.
    
    * Updated the version.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,9 @@
 ## 0.2.0
 
 - Added retrieving of reading plans.
+
+
+## 0.3.0
+
+- Fixed the missing API Key when retrieving reading plans.
+- Improved the source code formatting.
--- lib/src/data/data_sources/nlt_remote_data_source.dart
+++ lib/src/data/data_sources/nlt_remote_data_source.dart
@@ -23,19 +23,28 @@ abstract class NltRemoteDataSource extends RestfulDataSource {
   /// The [ref] is the reference(s) for the passages to be returned.
   /// More than one reference can be given, separated by semi-colons or commas.
   /// The [version] is the bible version of the passage that it will retrieve.
-  Future<String?> getPassages(String ref, {String version = 'NLT'});
+  Future<String?> getPassages(
+    String ref, {
+    String version = 'NLT',
+  });
 
   /// Executes text search to NLT API.
   ///
   /// The [text] is the text to search for.
   /// The [version] is the bible version of the passage that it will retrieve.
-  Future<String?> search(String text, {String version = 'NLT'});
+  Future<String?> search(
+    String text, {
+    String version = 'NLT',
+  });
 
   /// Executes parsing of the given reference.
   ///
   /// The [ref] is the reference string to parse.
   /// The [language] is the language of the result and defaults to english (en).
-  Future<List<dynamic>> parse(String ref, {String language = 'en'});
+  Future<List<dynamic>> parse(
+    String ref, {
+    String language = 'en',
+  });
 
   /// Retrieves the list of reading plans supported by the API.
   Future<List<Map<String, dynamic>>> plans();
